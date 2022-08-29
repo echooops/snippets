89406bb20092982fe037fd25ffb432679dec0f4d
木子兮 <35990133+echooops@users.noreply.github.com>
2022-08-29 14:17:27 +0800

    Update linux_shell_wapper.c

--- linux_shell_wapper.c
+++ linux_shell_wapper.c
@@ -7,45 +7,43 @@
  */
 int RemoveDir(const char *pathname, int recursive)
 {
-    char path[PATH_MAX] = { 0 };
-    if (rmdir(pathname) < 0) {
-        switch (errno) {
-            case ENOTDIR:
-            {
-                return unlink(pathname);
-            }
-            case ENOTEMPTY:
-            {
-                DIR *d = opendir(pathname);
-                if (d == NULL) {
-                    return -1;
-                }
-                struct dirent *dir;
-                while (dir = readdir(d), dir) {
-                    if (dir->d_name[0] == '.' && (dir->d_name[1] == '\0' || dir->d_name[1] == '.' && (dir->d_name[2] == '\0'))) {
-                        continue;   // 跳过 . and ..
-                    }
-                    if (snprintf(path, PATH_MAX, "%s/%s", pathname, dir->d_name) >= PATH_MAX) {
-                        closedir(d);
-                        return -1;
-                    }
-                    // 递归删除子目录
-                    if (recursive && RemoveDir(path, recursive) < 0) {
-                        closedir(d);
-                        return -1;
-                    }
-                }
-                closedir(d);
-
-                if (rmdir(pathname) < 0) {
-                    return -1;
-                }
-            } break;
-            default:
-            {
+    char path[PATH_MAX] = {0};
+    if (rmdir(pathname) == 0) {
+        return 0;
+    }
+    switch (errno) {
+        case ENOTDIR: {
+            return unlink(pathname);
+        }
+        case ENOTEMPTY: {
+            DIR *d = opendir(pathname);
+            if (d == NULL) {
                 return -1;
             }
+            struct dirent *dir;
+            while (dir = readdir(d), dir) {
+                if (dir->d_name[0] == '.' &&
+                    (dir->d_name[1] == '\0' || (dir->d_name[1] == '.' && dir->d_name[2] == '\0'))) {    // 偏移 2
+                    continue;  // 跳过 . and ..
+                }
+                if (snprintf_s(path, PATH_MAX, PATH_MAX - 1, "%s/%s", pathname, dir->d_name) < 0) {
+                    closedir(d);
+                    return -1;
+                }
+                // 递归删除子目录
+                if (recursive && RemoveDir(path, recursive) < 0) {
+                    closedir(d);
+                    return -1;
+                }
+            }
+            closedir(d);
+            if (rmdir(pathname) < 0) {
+                return -1;
+            }
+            break;
         }
+        default:
+            return -1;
     }
     return 0;
 }
