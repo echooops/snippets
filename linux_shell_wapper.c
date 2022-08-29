9094b892ef6730ae8be0c6611c36174e067af1e9
木子兮 <35990133+echooops@users.noreply.github.com>
2022-08-29 11:25:13 +0800

    Create linux_shell_wapper.c
    linux 常用命令封装

---
+++ linux_shell_wapper.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+/* 清空目录
+ * recursive: 0 清空当前目录文件, 1 递归清空子目录
+ */
+int RemoveDir(const char *pathname, int recursive)
+{
+    char path[PATH_MAX] = { 0 };
+    if (rmdir(pathname) < 0) {
+        switch (errno) {
+            case ENOTDIR:
+            {
+                return unlink(pathname);
+            }
+            case ENOTEMPTY:
+            {
+                DIR *d = opendir(pathname);
+                if (d == NULL) {
+                    return -1;
+                }
+                struct dirent *dir;
+                while (dir = readdir(d), dir) {
+                    if (dir->d_name[0] == '.' && (dir->d_name[1] == '\0' || dir->d_name[1] == '.' && (dir->d_name[2] == '\0'))) {
+                        continue;   // 跳过 . and ..
+                    }
+                    if (snprintf(path, PATH_MAX, "%s/%s", pathname, dir->d_name) >= PATH_MAX) {
+                        closedir(d);
+                        return -1;
+                    }
+                    // 递归删除子目录
+                    if (recursive && RemoveDir(path, recursive) < 0) {
+                        closedir(d);
+                        return -1;
+                    }
+                }
+                closedir(d);
+
+                if (rmdir(pathname) < 0) {
+                    return -1;
+                }
+            } break;
+            default:
+            {
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    return RemoveDir(argv[1], 1);
+}
